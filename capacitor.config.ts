f3d19548915f83edac56fb496fc7a0eae4d1851f
CC-Cristian <crielprog@gmail.com>
2024-03-29 09:25:48 -0500

    Final

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BDD-LIVE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
